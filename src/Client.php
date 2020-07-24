cc35b13dfb5ef98fcb804edeaa7ca31b0426cd97
Nechemya Kanelsky <mechemik770@gmail.com>
2020-07-24 12:06:08 +0300

    Fix: no query was passed

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,7 @@
 
 namespace Digitonic\IexCloudSdk;
 
+use Illuminate\Support\Arr;
 use GuzzleHttp\Client as Guzzle;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
@@ -38,10 +39,27 @@ class Client implements IEXCloud
     public function send(RequestInterface $request, array $options = []): ResponseInterface
     {
         try {
+            $options['query'] = $this->mergeQuery($request);
             return $this->client->send($request);
         }
         catch (ClientException $e) {
             throw WrongData::invalidValuesProvided($e->getMessage());
         }
+    }
+
+    /**
+     * Merge the global query with current query (if has it)
+     *
+     * @param RequestInterface $request
+     * @return array
+     */
+    private function mergeQuery(RequestInterface $request) {
+        $currentQueryArray = Arr::get($this->client->getConfig(), 'query');
+        $newQueryArray = [];
+        if($query = $request->getUri()->getQuery()) {
+            parse_str($query, $newQueryArray);
+        }
+
+        return array_merge($currentQueryArray, $newQueryArray);
     }
 }
